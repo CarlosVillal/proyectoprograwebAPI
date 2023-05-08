6890e2326a425ac7256ea82499df44095e47db2b
Yery Steven Marin Ortiz <111812790+YerySteven@users.noreply.github.com>
2023-05-08 01:26:58 -0600

    Cambios

---
+++ view/menuview.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="assets/images/x.png" rel="icon">
+    <title>Chuck Norris y datos random</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+<!--
+
+TemplateMo 569 Edu Meeting
+
+https://templatemo.com/tm-569-edu-meeting
+
+-->
+  </head>
+
+<body>
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="index.html" class="logo">
+                          Menú
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li><a href="meetings.html">Registrar Nuevo usuario</a></li>
+                          <li><a href="Index.php">Cerrar seción</a></li>
+                      </ul>        
+                      <a class='menu-trigger'>
+                          <span>Menu</span>
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <!-- ***** Main Banner Area Start ***** -->
+  <section class="section main-banner" id="top" data-section="section1">
+    <img src="assets/images/chuck.gif" alt="Chuck GIF" width="500" height="500" />
+
+
+      <div class="video-overlay header-text">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="caption">
+              <h6>"¡Bienvenidos al mundo de Chuck Norris, donde los chistes nunca mueren! En esta página web, encontrarás los chistes más divertidos y absurdos sobre la leyenda viva Chuck Norris. Además, te ofrecemos datos reales pero completamente irrelevantes sobre la vida cotidiana. ¡Aprenderás cosas que nunca imaginaste y te reirás a carcajadas con los chistes de Chuck Norris!"</h6>
+           
+          
+          </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </section>
+  <!-- ***** Main Banner Area End ***** -->
+
+  <section class="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="owl-service-item owl-carousel">
+          
+            <div class="item">
+              <div class="icon">
+                <img class="alicons" src="assets/images/chucx.svg" alt="">
+              </div>
+              <div class="down-content">
+              <h4><a href="chistes-chuck-norris.html">Chistes sobre Chuck Norris</a></h4>
+                <p>Es posible que algunas de las historias que escuches aquí sean verdaderas, pero probablemente ninguna de ellas se pueda verificar. De todos modos, ¡prepárate para reír y maravillarte ante las hazañas de este héroe mítico!</p>
+              </div>
+            </div>
+            
+            <div class="item">
+              <div class="icon">
+                <img class="alicons" src="assets/images/sonrisa.svg" alt="">
+              </div>
+              <div class="down-content">
+              <h4><a href="chistes-chuck-norris.html">Datos totalmente incesarios</a></h4>
+                <p>Bienvenidos a esta sección llena de información que nunca supiste que necesitabas, y probablemente nunca necesitarás.¡Así que siéntete libre de aprender cosas que nunca hubieras imaginado que podrían ser importantes</p>
+              </div>
+            </div>
+            
+            <div class="item">
+              <div class="icon">
+                <img id="alicons" src="assets/images/lista.svg" alt="">
+              </div>
+              <div class="down-content">
+              <h4><a href="chistes-chuck-norris.html">Chistes de usuarios</a></h4>
+                <p>Bienvenidos a la sección de chistes de usuarios, donde los ingeniosos y ocurrentes de la comunidad comparten sus mejores chistes para hacernos reír, prepárate para disfrutar de los chistes más divertidos de la comunidad.</p>
+              </div>
+            </div>
+            <div class="item">
+              <div class="icon">
+                <img id="alicons" src="assets/images/chat.svg" alt="">
+              </div>
+              <div class="down-content">
+              <h4><a href="chistes-chuck-norris.html">Agregar un chiste</a></h4>
+                <p>¿Tienes un chiste que crees que es tan bueno que debería ser compartido con todo el mundo? Bienvenido a la sección de "Agregar un chiste", donde puedes compartir tus mejores bromas con nuestra comunidad.</p>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="upcoming-meetings" id="meetings">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <div class="upcoming-meetings" id="meetings">
+
+    </div>
+  </section>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/owl-carousel.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+    <script src="assets/js/tabs.js"></script>
+    <script src="assets/js/video.js"></script>
+    <script src="assets/js/slick-slider.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script>
+        //according to loftblog tut
+        $('.nav li:first').addClass('active');
+
+        var showSection = function showSection(section, isAnimate) {
+          var
+          direction = section.replace(/#/, ''),
+          reqSection = $('.section').filter('[data-section="' + direction + '"]'),
+          reqSectionPos = reqSection.offset().top - 0;
+
+          if (isAnimate) {
+            $('body, html').animate({
+              scrollTop: reqSectionPos },
+            800);
+          } else {
+            $('body, html').scrollTop(reqSectionPos);
+          }
+
+        };
+
+        var checkSection = function checkSection() {
+          $('.section').each(function () {
+            var
+            $this = $(this),
+            topEdge = $this.offset().top - 80,
+            bottomEdge = topEdge + $this.height(),
+            wScroll = $(window).scrollTop();
+            if (topEdge < wScroll && bottomEdge > wScroll) {
+              var
+              currentId = $this.data('section'),
+              reqLink = $('a').filter('[href*=\\#' + currentId + ']');
+              reqLink.closest('li').addClass('active').
+              siblings().removeClass('active');
+            }
+          });
+        };
+
+        $('.main-menu, .responsive-menu, .scroll-to-section').on('click', 'a', function (e) {
+          e.preventDefault();
+          showSection($(this).attr('href'), true);
+        });
+
+        $(window).scroll(function () {
+          checkSection();
+        });
+    </script>
+</body>
+
+</body>
+</html>
