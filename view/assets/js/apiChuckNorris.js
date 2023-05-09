e14b3be862e597fa40dca91cb39610ff1543df8e
Yery Steven Marin Ortiz <111812790+YerySteven@users.noreply.github.com>
2023-05-09 00:31:20 -0600

    TraductorApi

--- view/assets/js/apiChuckNorris.js
+++ view/assets/js/apiChuckNorris.js
@@ -1,14 +1,32 @@
 
-    
-  // Seleccionar el elemento de la caja de texto
+// Seleccionar el elemento de la caja de texto
 const cajaTexto = document.getElementById("chisteChuckNorris");
-// Hacer la solicitud a la API de Chuck Norris
+
+// Hacer la solicitud a la API de uselessfacts
 fetch('https://api.chucknorris.io/jokes/random')
   .then(response => response.json())
   .then(data => {
-    // Establecer el valor de la caja de texto con el chiste obtenido
+    // Actualizar el valor de la caja de texto con el resultado obtenido
     cajaTexto.value = data.value;
+
+    // Hacer la solicitud a la API de traducción
+    const textoTraducir = data.value;
+    const idiomaOrigen = 'en';
+    const idiomaDestino = 'es';
+    const urlTraduccion = `https://api.mymemory.translated.net/get?q=${textoTraducir}&langpair=${idiomaOrigen}|${idiomaDestino}`;
+    fetch(urlTraduccion)
+      .then(response => response.json())
+      .then(data => {
+        // Actualizar el valor de la caja de texto con la traducción
+        const textoTraducido = data.responseData.translatedText;
+        cajaTexto.value = `${textoTraducir}\n\nTraducción:\n${textoTraducido}`;
+      })
+      .catch(error => {
+        console.error('Se produjo un error al obtener los datos de la API de traducción', error);
+      });
+
+  })
+  .catch(error => {
+    console.error('Se produjo un error al obtener los datos de la API de uselessfacts', error);
   });
-
-
 
--- view/assets/js/apiDatosRandom.js
+++ view/assets/js/apiDatosRandom.js
@@ -7,4 +7,24 @@ fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
   .then(data => {
     // Actualizar el valor de la caja de texto con el resultado obtenido
     cajaTexto.value = data.text;
+
+    // Hacer la solicitud a la API de traducción
+    const textoTraducir = data.text;
+    const idiomaOrigen = 'en';
+    const idiomaDestino = 'es';
+    const urlTraduccion = `https://api.mymemory.translated.net/get?q=${textoTraducir}&langpair=${idiomaOrigen}|${idiomaDestino}`;
+    fetch(urlTraduccion)
+      .then(response => response.json())
+      .then(data => {
+        // Actualizar el valor de la caja de texto con la traducción
+        const textoTraducido = data.responseData.translatedText;
+        cajaTexto.value = `${textoTraducir}\n\nTraducción:\n${textoTraducido}`;
+      })
+      .catch(error => {
+        console.error('Se produjo un error al obtener los datos de la API de traducción', error);
+      });
+
+  })
+  .catch(error => {
+    console.error('Se produjo un error al obtener los datos de la API de uselessfacts', error);
   });
